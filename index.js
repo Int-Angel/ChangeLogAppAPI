28813d38b685088b17f165834c5ee2c3702f00a1
Int-Angel <Padilla.angel88@gmail.com>
2022-12-03 15:16:19 -0600

    remove extra dependencies

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const cors = require("cors");
 const cookieSession = require("cookie-session");
-const bodyParser = require("body-parser");
 const config = require("./config");
 
 const authRoutes = require("./routes/auth-routes");
@@ -10,14 +9,11 @@ const projectRoutes = require("./routes/project-routes");
 const projectUpdateRoutes = require("./routes/update-routes");
 const pointRoutes = require("./routes/point-routes");
 
-//const { sequelize, AppUser } = require("./services/db");
-
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-//app.use(bodyParser.json());
 
 app.use(
   cookieSession({
@@ -32,25 +28,6 @@ app.use("/auth", authRoutes.routes);
 app.use("/project", projectRoutes.routes);
 app.use("/update", projectUpdateRoutes.routes);
 app.use("/point", pointRoutes.routes);
-
-/*
-app.get("/", (req, res) => {
-  res.json({ message: "Change log App" });
-});
-
-app.get("/test", async (req, res) => {
-  try {
-    const user = await AppUser.create({
-      username: "test user",
-      email: "test@gmail.com",
-      pass: "shouldbeencrypted",
-    });
-    res.json({ message: "User registered successfully!" });
-  } catch (error) {
-    res.status(500).send({ message: error.message });
-  }
-});
-*/
 
 app.listen(config.port, () =>
   console.log("App is listening on url http://localhost:" + config.port)
